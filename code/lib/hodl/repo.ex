ae625e3efe507ef1e4eb2106dfb8b2b0607c8235
Carlos S <sisnett19@gmail.com>
2021-02-05 15:35:13 -0500

    eliminate pool size

--- code/lib/hodl/repo.ex
+++ code/lib/hodl/repo.ex
@@ -1,8 +1,7 @@
 defmodule Hodl.Repo do
   use Ecto.Repo,
     otp_app: :hodl,
-    adapter: Ecto.Adapters.Postgres,
-    pool_size: 40
+    adapter: Ecto.Adapters.Postgres
 
     def init(_type, config) do
       {:ok, Keyword.put(config, :url, System.get_env("HODL_DATABASE_URL"))}
