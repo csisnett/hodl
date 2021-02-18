aec809ece307431b464730de187c3ca5e21f57ff
Carlos S <sisnett19@gmail.com>
2021-02-17 23:16:07 -0500

    add coinmarketcap_id to Coin schema

--- code/lib/hodl/portfolio/coin.ex
+++ code/lib/hodl/portfolio/coin.ex
@@ -1,18 +1,23 @@
 defmodule Hodl.Portfolio.Coin do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Hodl.Portfolio.Coin
 
   schema "coins" do
     field :name, :string
     field :symbol, :string
     field :uuid, Ecto.ShortUUID, autogenerate: true
+    field :coinmarketcap_id, :integer
+    belongs_to :platform, Coin
+    field :token_address, :string
     timestamps()
   end
 
   @doc false
   def changeset(coin, attrs) do
     coin
-    |> cast(attrs, [:name, :symbol])
-    |> validate_required([:name, :symbol])
+    |> cast(attrs, [:name, :symbol, :coinmarketcap_id, :platform_id, :token_address])
+    |> validate_required([:name, :symbol, :coinmarketcap_id])
+    |> foreign_key_constraint(:platform_id)
   end
 end
