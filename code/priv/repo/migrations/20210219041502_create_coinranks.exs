c65daad86ef0cac4a37b705b32131343611fa047
Carlos S <sisnett19@gmail.com>
2021-02-18 23:25:36 -0500

    create coinrank migration

---
+++ code/priv/repo/migrations/20210219041502_create_coinranks.exs
@@ -0,0 +1,13 @@
+defmodule Hodl.Repo.Migrations.CreateCoinranks do
+  use Ecto.Migration
+
+  def change do
+    create table(:coinranks) do
+      add :position, :integer, null: false
+      add :coin_id, references(:coins, on_delete: :delete_all), null: false
+      add :ranking_id, references(:rankings, on_delete: :delete_all), null: false
+      timestamps()
+    end
+    create unique_index(:coinranks, [:coin_id, :ranking_id])
+  end
+end
