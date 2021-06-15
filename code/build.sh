ca2975bb5be27c77fdbd964fe14c9fc2b03d5a8d
Carlos S <sisnett19@gmail.com>
2021-06-15 01:56:56 -0500

    modify build.sh to the one who works

--- code/build.sh
+++ code/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
@@ -11,5 +8,9 @@ npm install --prefix ./assets
 npm run deploy --prefix ./assets
 mix phx.digest
 
-# Build the release and overwrite the existing release directory
-MIX_ENV=prod mix release --overwrite
+# Remove the existing release directory and build the release
+rm -rf "_build"
+MIX_ENV=prod mix release
+
+#for auto dB migration upon deploy
+MIX_ENV=prod mix ecto.migrate
