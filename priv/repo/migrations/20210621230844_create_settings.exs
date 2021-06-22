1cd40ac16081ba66c42f03af2f7e5bc45841d770
Carlos S <sisnett19@gmail.com>
2021-06-22 13:01:45 -0500

    create settings table

---
+++ priv/repo/migrations/20210621230844_create_settings.exs
@@ -0,0 +1,18 @@
+defmodule Hodl.Repo.Migrations.CreateSettings do
+  use Ecto.Migration
+
+  def change do
+    create table(:settings) do
+      add :setting_key, :string, null: false
+      add :value, :string, null: false
+      add :uuid, :uuid, null: false
+
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      timestamps()
+    end
+    create index(:settings, [:user_id])
+    create unique_index(:settings, [:uuid])
+    create unique_index(:settings, [:user_id, :setting_key])
+
+  end
+end
