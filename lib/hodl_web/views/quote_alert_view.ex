f7a9d6ddc5cb50f2353816d16ecf4ca248fd7b78
Carlos S <sisnett19@gmail.com>
2021-07-02 18:51:46 -0500

    add more things in quote_alert.json fn

--- lib/hodl_web/views/quote_alert_view.ex
+++ lib/hodl_web/views/quote_alert_view.ex
@@ -2,18 +2,22 @@ defmodule HodlWeb.QuoteAlertView do
   use HodlWeb, :view
   alias HodlWeb.QuoteAlertView
 
-  def render("index.json", %{quote_alert: quote_alert}) do
-    %{data: render_many(quote_alert, QuoteAlertView, "quote_alert.json")}
+  def render("index.json", %{quote_alerts: quote_alerts}) do
+    %{quote_alerts: render_many(quote_alerts, QuoteAlertView, "quote_alert.json")}
   end
 
   def render("show.json", %{quote_alert: quote_alert}) do
-    %{data: render_one(quote_alert, QuoteAlertView, "quote_alert.json")}
+    render_one(quote_alert, QuoteAlertView, "quote_alert.json")
   end
 
   def render("quote_alert.json", %{quote_alert: quote_alert}) do
-    %{id: quote_alert.id,
+    %{uuid: quote_alert.uuid,
+      price_usd: quote_alert.price_usd,
       uuid: quote_alert.uuid,
-      price_usd: quote_alert.price_usd}
+      coin_name: quote_alert.coin_name,
+      coin_symbol: quote_alert.coin_symbol,
+      comparator: quote_alert.comparator,
+      active: quote_alert.active?}
   end
 
 end
