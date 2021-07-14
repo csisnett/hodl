841943039e063cc56beb9697bedd231beafe19a5
Carlos S <sisnett19@gmail.com>
2021-07-14 18:13:57 -0500

    print out different alerts per coin

--- lib/hodl_web/views/quote_alert_view.ex
+++ lib/hodl_web/views/quote_alert_view.ex
@@ -7,6 +7,96 @@ defmodule HodlWeb.QuoteAlertView do
     |> Enum.join(",")
   end
 
+  def order_by_coin([], result) do
+    result
+  end
+
+  def order_by_coin(quote_alerts, result) do
+    [first | rest] = quote_alerts
+    coin_uuid = first.coin_uuid
+    alerts_for_coin = Enum.filter(quote_alerts, fn quote_alert -> quote_alert.coin_uuid == coin_uuid end)
+    other_coin_alerts = Enum.filter(quote_alerts, fn quote_alert -> quote_alert.coin_uuid != coin_uuid end)
+    order_by_coin(other_coin_alerts, result ++ [alerts_for_coin]) #Maintains a seperate list for each coin
+  end
+
+  def output_alerts([], result) do
+    html_escape(result)
+  end
+
+  def coin_category([], result) do
+    result
+  end
+
+  def single_alert_html(quote_alert, acc) do
+    # Because we get the coin names and symbols from external sources we need to make sure
+    # There's nothing in them that could be used for attacks
+    with {:safe, coin_name} <- html_escape(quote_alert.coin_name),
+         {:safe, coin_symbol} <- html_escape(quote_alert.coin_symbol)
+
+    do
+      coin_name_raw = {:safe, coin_name} |> safe_to_string()
+      coin_symbol_raw = {:safe, coin_symbol} |> safe_to_string()
+    alert_string = """
+    <b-col cols="4">
+    <alert
+    coin_name="#{coin_name_raw}"
+    coin_symbol="#{coin_symbol_raw}"
+    comparator="#{quote_alert.comparator}"
+    uuid="#{quote_alert.uuid}"
+    price_usd="#{quote_alert.price_usd}"
+    coin_uuid="#{quote_alert.coin.uuid}">
+
+    </alert>
+    </b-col>
+
+    """
+    alert_string <> acc
+    else
+        {:unsafe, _} -> acc
+    end
+  end
+
+    # [QuoteAlert{coin_uuid: uuid}, QuoteAlert{coin_uuid: uuid}, ..] -> String
+  # Receives a list of quote alerts with the same coin and outputs the String html for each alert
+  def coin_category(quote_alerts, acc) do
+    [first_alert | rest] = quote_alerts
+    with {:safe, coin_name} <- html_escape(first_alert.coin_name)
+    do
+      coin_name_raw = {:safe, coin_name} |> safe_to_string()
+
+    initial_coin_string = """
+
+    <h3 style="text-align: center;"> #{coin_name_raw} alerts </h3>
+    <b-container >
+    <b-row style="min-width: 600px; position: relative;">
+    """
+    all_alerts_string = Enum.reduce(quote_alerts, "", &single_alert_html/2)
+    ending_string = """
+
+    </b-row>
+    </b-container>
+    <br>
+    """
+
+    initial_coin_string <> all_alerts_string <> ending_string <> acc
+    else
+      {:unsafe, _} -> "<h3> Error displaying coin #{first_alert.coin_uuid}, please notify the admin </h3>"
+    end
+  end
+
+  def get_alerts() do
+    [user | _] = Hodl.Repo.all(Hodl.Users.User)
+    quotealerts = Hodl.Portfolio.list_user_quotealerts_out(user)
+  end
+
+  # [QuoteAlerts] -> HTML
+  # Output the quote alerts categorized by coin
+  def output_alerts(quote_alerts) do
+    alerts_per_coin = order_by_coin(quote_alerts, [])
+    all_alerts_string = Enum.reduce(alerts_per_coin, "", &coin_category/2)
+    raw(all_alerts_string)
+  end
+
   def render("index.json", %{quote_alerts: quote_alerts}) do
     %{quote_alerts: render_many(quote_alerts, QuoteAlertView, "quote_alert.json")}
   end
