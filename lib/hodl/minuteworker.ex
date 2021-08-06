18a3c66ed9d433e929449925f86c9876bc9379fa
Carlos Sisnett <daniel@pelable.com>
2021-08-06 02:16:30 -0500

    remove attempts, from minute worker

--- lib/hodl/minuteworker.ex
+++ lib/hodl/minuteworker.ex
@@ -3,21 +3,10 @@ defmodule Hodl.MinuteWorker do
     use Oban.Worker,
       queue: :repetitive,
       priority: 1,
-      max_attempts: 2,
       tags: ["business"],
       unique: [fields: [:queue, :worker]]
 
     @impl Oban.Worker
-    def perform(%Oban.Job{attempt: attempt}) when attempt > 1 do
-
-      message = "This is attempt # #{attempt}!"
-      IO.inspect(message)
-      IO.inspect(DateTime.utc_now())
-
-      Portfolio.initiate_quote_retrieval()
-      :ok
-    end
-
     def perform(job) do
       case Application.fetch_env!(:hodl, :development) do
         "false" ->
