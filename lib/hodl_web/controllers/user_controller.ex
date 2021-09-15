5158a73b703edde38e574c6b7cb01556bb80cd6e
Carlos Sisnett <daniel@pelable.com>
2021-09-15 13:57:57 -0500

    sign out fn in user controller

--- lib/hodl_web/controllers/user_controller.ex
+++ lib/hodl_web/controllers/user_controller.ex
@@ -1,10 +1,9 @@
 defmodule HodlWeb.UserController do
   use HodlWeb, :controller
-  
+
   alias Hodl.Users.User
   alias Hodl.Accounts
   alias Hodl.Accounts.Setting
-  alias HodlWeb.Pow
 
   def new_account(conn, _params) do
     render(conn, "new-account.html")
@@ -12,14 +11,21 @@ defmodule HodlWeb.UserController do
 
   def create_account(conn, %{"user" => user_params}) do
     case Accounts.create_user(user_params) do
-      {:ok, user} -> 
-      conn = Pow.Plug.sign_in_user_from_server(conn, user)
+      {:ok, user} ->
+      conn = HoldWeb.Pow.Plug.sign_in_user_from_server(conn, user)
       |> redirect(to: "/new-schedule")
 
       {:error, changeset} ->
         conn
         |> redirect(to: "/new-account?retry=true")
     end
+  end
+
+  def sign_out(conn, _params) do
+    conn
+    |> Plug.Conn.delete_resp_cookie("_hodl_key")
+    |> Plug.Conn.delete_resp_cookie("hodl_persistent_session")
+    |> redirect(to: "/session/new")
   end
 
 end
