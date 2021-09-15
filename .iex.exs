e5e75d81d89e9a8744b3f406ee217aa54cfd9920
Carlos Sisnett <daniel@pelable.com>
2021-09-15 13:58:18 -0500

    add alerttrigger to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,6 @@
 alias Hodl.Repo
 alias Hodl.Users.User
 alias Hodl.Portfolio
-alias Hodl.Portfolio.{Coin, Hodlschedule, Quote, Cycle, Ranking, Coinrank, QuoteAlert}
+alias Hodl.Portfolio.{Coin, Hodlschedule, Quote, Cycle, Ranking, Coinrank, QuoteAlert, AlertTrigger}
 alias Hodl.Accounts
 alias Hodl.Accounts.{Plan, Subscription, Setting}
